a5920266182d593f347291862bdd80b8c0de603d
David Mydlarz <coldfront@gmail.com>
2021-11-21 10:53:11 +0000

    add types to not found exception filter

--- src/exception/notfoundexception.filter.ts
+++ src/exception/notfoundexception.filter.ts
@@ -1,14 +1,21 @@
 import { ArgumentsHost, Catch, ExceptionFilter, NotFoundException } from "@nestjs/common";
-import { HttpArgumentsHost } from "@nestjs/common/interfaces";
+import { Request, Response } from 'express';
 
 @Catch(NotFoundException)
 export class NotFoundExceptionFilter implements ExceptionFilter {
     catch(exception: NotFoundException, host: ArgumentsHost) {
-        const ctx: HttpArgumentsHost = host.switchToHttp();
-        const response = ctx.getResponse();
-        response.jsonp({
-            statusCode: 404,
-            message: 'not found'
-        })
+        const ctx = host.switchToHttp();
+        const response = ctx.getResponse<Response>();
+        const request = ctx.getRequest<Request>();
+        const status = exception.getStatus();
+
+        response
+            .status(status)
+            .json({
+                statusCode: status,
+                timestamp: new Date().toISOString(),
+                // path: request.url,
+                message: 'not found'
+            });
     }
 }
