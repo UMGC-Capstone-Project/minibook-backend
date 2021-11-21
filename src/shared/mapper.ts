a7cdc075737362303b998586935e780944922d91
David Mydlarz <coldfront@gmail.com>
2021-11-21 17:19:47 +0000

    move out typeorm from json due to strange entity issue, start implmenting basic auth for database storage.

--- src/shared/mapper.ts
+++ src/shared/mapper.ts
@@ -2,7 +2,7 @@ import { UserDto } from "src/dto/UserDto";
 import { UserEntity } from "src/entity/user.entity";
 
 export const toUserDto = (data: UserEntity): UserDto => {
-    const {id, displayname, email} = data;
-    let userDto: UserDto = { id, displayname, email};
+    const { id, displayname, email } = data;
+    let userDto: UserDto = { id, displayname, email };
     return userDto;
 }
