74819171d5b5e6332b3ccc94a6ae19860a3d5905
David Mydlarz <coldfront@gmail.com>
2021-11-22 08:58:55 +0000

    update for digital ocean apps testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,17 +8,19 @@ import { UsersModule } from './user/users.module';
 import { FeedModule } from './feed/feed.module';
 import { PostEntity } from './feed/entities/post.entity';
 import { UserEntity } from './user/entities/user.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot(
       {
         type: "mysql",
-        host: "db",
-        port: 3306,
-        username: "root",
-        password: "db-q5n2g",
-        database: "minibook_development",
+        host: process.env.DATABASE_HOST ,
+        port: parseInt(process.env.DATABASE_PORT, 10),
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_DATABASE,
         synchronize: true,
         entities: [
           UserEntity,
