ed6c886281e05c071de2ee76df645a93abe9c3b9
David Mydlarz <coldfront@gmail.com>
2021-11-25 14:50:59 +0000

    add base methods for following a user

--- src/friend/controllers/friend.controller.ts
+++ src/friend/controllers/friend.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, NotImplementedException } from '@nestjs/common';
 
 @Controller('friends')
-export class FriendsController {}
+export class FriendsController {
+
+
+    @Get(':id/follow')
+    follow() {
+        throw new Error('Method not implemented.');
+    }
+
+    @Get(':id/unfollow')
+    unfollow() {
+        throw new Error('Method not implemented.');
+    }
+
+    @Get(':id/block')
+    block() {
+        throw new Error('Method not implemented.');
+    }
+
+    @Get(':id/unblock')
+    unblock() {
+        throw new Error('Method not implemented.');
+    }
+}
